060da308c91486adf7295903d218fcd68bb62729
Biswojit Pradhan <biswojitpradhan02@gmail.com>
2026-03-28 20:26:27 +0530

    Create ATMException.py

---
+++ ATMException.py
@@ -0,0 +1,17 @@
+/*
+====================================================================
+This file defines 3 custom exception classes for an ATM system.
+
+    🔴 Raised when something goes wrong during deposit
+    (e.g. depositing negative or zero amount)
+    🔴 Raised when something goes wrong during withdrawal
+    (e.g. withdrawing invalid amount)
+    🔴 Raised when balance is not enough to withdraw
+    (e.g. withdrawing ₹5000 when balance is ₹1000)
+===================================================================
+*/
+
+
+class DepositError(Exception):pass
+class WithdrawError(Exception):pass
+class InsufficientAmountError(Exception):pass

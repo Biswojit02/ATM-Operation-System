07fe24a8f09fda8c9e0cf944dc4973590988df86
Biswojit Pradhan <biswojitpradhan02@gmail.com>
2026-03-28 20:31:09 +0530

    Create ATMOperation.py

---
+++ ATMOperation.py
@@ -0,0 +1,18 @@
+/*
+=============================================================== 
+- Created `menu()` function to display ATM operations
+- Displays 4 options: Deposit, Withdraw, Balance Inquiry, Exit
+- Formatted with separators for clean console UI
+- Base structure for ATM simulation program
+===============================================================                           
+*/
+                           
+def menu():
+    print("="*50)
+    print("\t\tATM Operation")
+    print("="*50)
+    print("\t1.Deposit")
+    print("\t2.Withdraw")
+    print("\t3.Balance Inquiry")
+    print("\t4.Exit")
+    print("-"*50)
